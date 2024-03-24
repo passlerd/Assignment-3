9c0107e766a2f1cd3e7c7618401fb2cd3f09ff3c
passlerd <passlerd@myumanitoba.ca>
2024-03-24 12:45:52 -0500

    implement when s1 is in s2 and when i is greater than pos (GREEN)

--- src/main/java/MyString.java
+++ src/main/java/MyString.java
@@ -5,9 +5,20 @@ public class MyString {
         }
         //create two pointers: i in s1 and j in s2, and move j pointer along s2 if the string so far matches s1
         int j = 0;
+        int startIndex = -1;
         for (int i = 0; i < s1.length(); i++){
             if (s1.charAt(i) == s2.charAt(j)){
-                j++;
+                //if startIndex is -1, it is the beginning of the potential matching substring in s1
+                if(startIndex == -1){
+                    startIndex = i;
+                }
+                if(j==s2.length() -1){
+                    return startIndex;
+                }
+                //do not increase the pointer for s2 beyond the length of s2
+                if(j< s2.length() -1){
+                    j++;
+                }
             }
         }
         //if we have traversed all of s1 and s2 pointer has not moved to end of s2, s2 is not in s1
