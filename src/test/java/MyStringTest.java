8dc9a35f8f1e5c8787c056e28e6ceaa1a49fd104
passlerd <passlerd@myumanitoba.ca>
2024-03-23 22:32:10 -0500

    implement test for when s2 is null (RED)

--- src/test/java/MyStringTest.java
+++ src/test/java/MyStringTest.java
@@ -14,4 +14,13 @@ public class MyStringTest {
         Assert.assertEquals(-1, i);
     }
 
+    @Test
+    public void tests2Null(){
+        String s1 = "string";
+        String s2 = null;
+        int pos = 1;
+        int i = ms.indexOfString(s1,s2,pos);
+        Assert.assertEquals(-1, i);
+    }
+
 }
