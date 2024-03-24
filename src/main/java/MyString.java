bd4d454b355f5f103326b1970bfb91f37b59d961
passlerd <passlerd@myumanitoba.ca>
2024-03-23 22:33:29 -0500

    implementation for when s2 is null (GREEN)

--- src/main/java/MyString.java
+++ src/main/java/MyString.java
@@ -1,6 +1,6 @@
 public class MyString {
     public int indexOfString(String s1, String s2, int pos){
-        if (s1 == null){
+        if (s1 == null || s2 == null){
             return -1;
         }
         return -2;
