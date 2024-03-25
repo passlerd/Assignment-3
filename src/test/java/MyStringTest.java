fa5dd51de75e85d8d77266dfb51a77b8d73844c8
passlerd <passlerd@myumanitoba.ca>
2024-03-24 20:58:54 -0500

    test of replace when s contains s1 once (RED)

--- src/test/java/MyStringTest.java
+++ src/test/java/MyStringTest.java
@@ -134,4 +134,13 @@ public class MyStringTest {
         String actual = ms.replace(s,s1,s2);
         Assert.assertEquals("string", actual);
     }
+
+    @Test
+    public void replacesContainsS1Once(){
+        String s = "string";
+        String s1 = "t";
+        String s2 = "cat";
+        String actual = ms.replace(s,s1,s2);
+        Assert.assertEquals("scatring", actual);
+    }
 }
