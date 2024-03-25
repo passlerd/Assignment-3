fe2765899ea7cb9d9a12a15f38449c05fab256af
passlerd <passlerd@myumanitoba.ca>
2024-03-24 22:21:17 -0500

    implementation of replace when s contains s1 multiple times (GREEN)

--- src/main/java/MyString.java
+++ src/main/java/MyString.java
@@ -31,27 +31,32 @@ public class MyString {
     }
 
     public String replace(String s, String s1, String s2) {
-        if (s == null || s1==null || s2==null){
+        if (s == null || s1 == null || s2 == null) {
             return null;
-        } else if (s1.equals("")){
+        } else if (s1.equals("")) {
             return s;
-        }else{
-            int indexs1 = indexOfString(s,s1,0);
-            if(indexs1 == -1){
-                return s;
-            }
-            else{
-                StringBuilder returnString = new StringBuilder();
-                for(int i = 0; i<indexs1; i++ ){
+        } else {
+            StringBuilder returnString = new StringBuilder();
+            //sIndex is pointer where we are traversing string s
+            int sIndex = 0;
+            //find index where the first s1 is in s
+            int indexs1 = indexOfString(s, s1, 0);
+            //while there are still strings of s1 in s
+            while (indexs1 != -1) {
+                //add characters between elements of s1
+                for (int i = sIndex; i < indexs1; i++) {
                     returnString.append(s.charAt(i));
                 }
+                //add the string to replace s1 with
                 returnString.append(s2);
-                for (int i = indexs1 +1; i< s.length(); i++){
-                    returnString.append(s.charAt(i));
-                }
-                return returnString.toString();
+                sIndex = indexs1 + 1;
+                //find next string s1 in s
+                indexs1 = indexOfString(s, s1, sIndex);
             }
-
+            for (int i = sIndex; i < s.length(); i++) {
+                returnString.append(s.charAt(i));
+            }
+            return returnString.toString();
         }
     }
 }
